79fa4c8f26e5dd47e9a673a3d8b2f4b7acb0f288
kyounghwan kim <79563142+KyongHwan-Kim@users.noreply.github.com>
2021-10-01 19:34:27 +0900

    Update algorithm_test.py

--- back_end_algorithm/algorithm_test.py
+++ back_end_algorithm/algorithm_test.py
@@ -8,97 +8,101 @@ running_speed = 2.5 #(m/s)
 #==========Main_Gate=============[building][floor][kind][Kind_number][gate_number]
 graph[str([4,0,0,0])] = {} #"MainGate_1"
 graph[str([4,0,0,0])][str([4,0,0,1])] = 15.67
+graph[str([4,0,0,0])][str([31,0,0,2])] = 15.67
 graph[str([4,0,0,0])][str([30,0,0,0])] = 29.75
 graph[str([4,0,0,1])] = {}
 graph[str([4,0,0,1])][str([30,0,0,0])] = 32.79
+graph[str([4,0,0,1])][str([4,0,0,0])] = 32.79
 
 #===========Road_str=============[building][floor][kind][Kind_number][gate_number]
 graph[str([30,0,0,0])] = {} #"Road_str1_node1"
-graph[str([30,0,0,0])][str([30,0,0,1])] = 14
-graph[str([30,0,0,0])][str([2,0,0,0])] = 4
-graph[str([30,0,0,0])][str([2,0,1,0])] = 4
-graph[str([30,0,0,0])][str([30,5,0,0])] = 4
+graph[str([30,0,0,0])][str([30,0,0,1])] = 43.92
+graph[str([30,0,0,0])][str([2,0,0,0])] = 16.54
+graph[str([30,0,0,0])][str([2,0,3,0])] = 19.52
+graph[str([30,0,0,0])][str([30,5,0,0])] = 19.88
 
 graph[str([30,0,0,1])] = {} #"Road_str1_node2
-graph[str([30,0,0,1])][str([30,1,0,0])] = 7
-graph[str([30,0,0,1])][str([30,2,0,0])] = 7
+graph[str([30,0,0,1])][str([30,1,0,0])] = 50.06
+graph[str([30,0,0,1])][str([30,2,0,0])] = 30.67
 
 
 graph[str([30,1,0,0])] = {} #"Road_str2_node1
-graph[str([30,1,0,0])][str([30,1,0,1])] = 9
-graph[str([30,1,0,0])][str([30,5,0,7])] = 9
-graph[str([30,1,0,0])][str([30,5,0,2])] = 9
-graph[str([30,1,0,0])][str([30,2,0,1])] = 9
+graph[str([30,1,0,0])][str([30,1,0,1])] = 37.15
+graph[str([30,1,0,0])][str([30,5,0,7])] = 15.68
+graph[str([30,1,0,0])][str([30,5,0,2])] = 31.66
+graph[str([30,1,0,0])][str([30,2,0,1])] = 17.17
 
 graph[str([30,1,0,1])] = {} #"Road_str2_node2
-graph[str([30,1,0,1])][str([30,2,0,2])] = 9
-graph[str([30,1,0,1])][str([30,3,0,0])] = 9
-graph[str([30,1,0,1])][str([30,3,0,1])] = 9
+graph[str([30,1,0,1])][str([30,2,0,2])] = 12.45
+graph[str([30,1,0,1])][str([30,3,0,0])] = 18.05
+graph[str([30,1,0,1])][str([30,3,0,1])] = 18.21
 
 graph[str([30,2,0,0])] = {} #"Road_str3_node1
-graph[str([30,2,0,0])][str([30,2,0,1])] = 15
+graph[str([30,2,0,0])][str([30,2,0,1])] = 39.75
 graph[str([30,2,0,1])] = {} #"Road_str3_node2
-graph[str([30,2,0,1])][str([1,0,0,0])] = 13
-graph[str([30,2,0,1])][str([30,2,0,2])] = 13
+graph[str([30,2,0,1])][str([1,0,0,0])] = 8.19
+graph[str([30,2,0,1])][str([30,2,0,2])] = 26.52
 graph[str([30,2,0,2])] = {} #"Road_str3_node3
-graph[str([30,2,0,2])][str([1,0,0,1])] = 13
+graph[str([30,2,0,2])][str([1,0,0,1])] = 10.83
 
 graph[str([30,3,0,0])] = {}
-graph[str([30,3,0,0])][str([30,3,0,2])] = 14
+graph[str([30,3,0,0])][str([30,3,0,2])] = 17.85
 graph[str([30,3,0,1])] = {}
-graph[str([30,3,0,1])][str([30,3,0,2])] = 14
+graph[str([30,3,0,1])][str([30,3,0,2])] = 17.05
 graph[str([30,3,0,2])] = {}
-graph[str([30,3,0,2])][str([30,3,0,3])] = 14
+graph[str([30,3,0,2])][str([30,3,0,3])] = 13.76
 graph[str([30,3,0,3])] = {}
-graph[str([30,3,0,3])][str([30,3,0,4])] = 14
-graph[str([30,3,0,3])][str([30,3,0,5])] = 14
+graph[str([30,3,0,3])][str([30,3,0,4])] = 9.57
+graph[str([30,3,0,3])][str([30,3,0,5])] = 30.17
 graph[str([30,3,0,4])] = {}
-graph[str([30,3,0,4])][str([30,3,0,7])] = 14
+graph[str([30,3,0,4])][str([30,3,0,7])] = 57.31
 graph[str([30,3,0,5])] = {}
-graph[str([30,3,0,5])][str([30,3,0,6])] = 14
+graph[str([30,3,0,5])][str([30,3,0,6])] = 5.52
 graph[str([30,3,0,6])] = {}
-graph[str([30,3,0,6])][str([30,3,0,8])] = 14
-graph[str([30,3,0,6])][str([30,3,0,7])] = 14
+graph[str([30,3,0,6])][str([30,3,0,8])] = 15.50
+graph[str([30,3,0,6])][str([30,3,0,7])] = 29.73
 graph[str([30,3,0,7])] = {}
-graph[str([30,3,0,7])][str([0,0,0,0])] = 14
-graph[str([30,3,0,7])][str([30,5,0,4])] = 14
-graph[str([30,3,0,7])][str([2,-1,0,1])] = 14
+graph[str([30,3,0,7])][str([0,0,0,0])] = 7.10
+graph[str([30,3,0,7])][str([30,5,0,4])] = 64.97
+graph[str([30,3,0,7])][str([2,-1,0,1])] = 71.91
 graph[str([30,3,0,8])] = {}
-graph[str([30,3,0,8])][str([30,3,0,7])] = 14
+graph[str([30,3,0,8])][str([30,3,0,7])] = 24.06
 
 graph[str([30,5,0,0])] = {}
-graph[str([30,5,0,0])][str([30,5,0,1])] = 123
+graph[str([30,5,0,0])][str([30,5,0,1])] = 23.34
 graph[str([30,5,0,1])] = {}
-graph[str([30,5,0,1])][str([30,5,0,2])] = 123
+graph[str([30,5,0,1])][str([30,5,0,2])] = 29.36
 graph[str([30,5,0,2])] = {}
-graph[str([30,5,0,2])][str([30,5,0,8])] = 123
-graph[str([30,5,0,2])][str([30,5,0,3])] = 123
-graph[str([30,5,0,2])][str([30,5,0,7])] = 123
+graph[str([30,5,0,2])][str([30,5,0,8])] = 23.8
+graph[str([30,5,0,2])][str([30,5,0,3])] = 20.96
+graph[str([30,5,0,2])][str([30,5,0,7])] = 29.30
 graph[str([30,5,0,3])] = {}
-graph[str([30,5,0,3])][str([30,5,0,5])] = 213
-graph[str([30,5,0,3])][str([30,5,0,4])] = 5234
+graph[str([30,5,0,3])][str([30,5,0,5])] = 8.18
+graph[str([30,5,0,3])][str([30,5,0,4])] = 36.34
 graph[str([30,5,0,4])] = {}
 graph[str([30,5,0,5])] = {}
-graph[str([30,5,0,5])][str([30,5,0,6])] = 54
+graph[str([30,5,0,5])][str([30,5,0,6])] = 30.22
 graph[str([30,5,0,6])] = {}
-graph[str([30,5,0,6])][str([3,0,0,1])] = 54
-graph[str([30,5,0,6])][str([30,3,0,2])] = 54
+graph[str([30,5,0,6])][str([3,0,0,1])] = 2.87
+graph[str([30,5,0,6])][str([30,3,0,2])] = 35.50
 graph[str([30,5,0,7])] = {}
-graph[str([30,5,0,7])][str([30,3,0,0])] = 213
-graph[str([30,5,0,7])][str([3,0,0,0])] = 213
+graph[str([30,5,0,7])][str([30,3,0,0])] = 28.24
+graph[str([30,5,0,7])][str([3,0,0,0])] = 9.82
 graph[str([30,5,0,8])] = {}
-graph[str([30,5,0,8])][str([30,5,0,3])] = 123
-graph[str([30,5,0,8])][str([2,-1,0,0])] = 123
+graph[str([30,5,0,8])][str([30,5,0,3])] = 19.44
+graph[str([30,5,0,8])][str([2,-1,0,0])] = 12.59
 
 graph[str([31,0,0,0])] = {}
-graph[str([31,0,0,0])][str([31,0,0,1])] = 123
-graph[str([31,0,0,0])][str([0,0,0,1])] = 123
+graph[str([31,0,0,0])][str([31,0,0,1])] = 16.88
+graph[str([31,0,0,0])][str([0,0,0,1])] = 32.94
 graph[str([31,0,0,1])] = {}
-graph[str([31,0,0,1])][str([31,0,0,2])] = 123
-graph[str([31,0,0,1])][str([30,3,0,7])] = 12323
+graph[str([31,0,0,1])][str([31,0,0,2])] = 81.73
+graph[str([31,0,0,1])][str([31,0,0,0])] = 81.73 # 추가
+graph[str([31,0,0,1])][str([30,3,0,7])] = 67.61
 graph[str([31,0,0,2])] = {}
-graph[str([31,0,0,2])][str([4,0,0,0])] = 123
-graph[str([31,0,0,2])][str([2,0,0,1])] = 123
+graph[str([31,0,0,2])][str([4,0,0,0])] = 50.36
+graph[str([31,0,0,2])][str([31,0,0,1])] = 50.36 # 추가
+graph[str([31,0,0,2])][str([2,0,0,2])] = 12.47
 
 #===========StudentHall===========[building][floor][kind][Kind_number][gate_number]
 #Gate
@@ -108,7 +112,7 @@ graph[str([2,-1,0,0])] = {} # B1 gate_2
 graph[str([2,-1,0,1])] = {} # B1 gate_3
 graph[str([2,0,0,2])] = {} #"StudentHall_1floor_gate_3"
 #Shop
-graph[str([2,0,1,0])] = {} #ParisBagate
+graph[str([2,0,3,0])] = {} #ParisBagate
 
 
 #===========DaeYangAI===========[building][floor][kind][Kind_number][gate_number]
@@ -119,7 +123,6 @@ graph[str([0,0,0,2])] = {} #"DaeYangAI_1floor_gate_3"
 #=============Gunja=============[building][floor][kind][Kind_number][gate_number]
 graph[str([1,0,0,0])] = {} #"Gunja_1floor_gate_1"
 graph[str([1,0,0,1])] = {} #"Gunja_1floor_gate_2"
-graph[str([1,0,0,0])][str([1,0,0,0])] = 23
 
 #==========Sejong_gaun==========[building][floor][kind][Kind_number][gate_number]
 graph[str([3,0,0,0])] = {} # sejong_1floor_gate_1"
@@ -151,10 +154,11 @@ def dijkstra_one(graph, start, end):
         path_output += distances[path][1] + '->'
         path = distances[path][1]
     path_output += start
+    print(path_output)
     return distance
 
-total_distance = dijkstra_one(graph, str([4,0,0,0]), str([30,2,0,1]))
-print('total distance : '+ str(dijkstra_one(graph, str([4,0,0,0]), str([30,2,0,1])))+'m')
+total_distance = dijkstra_one(graph, str([4,0,0,1]), str([0,0,0,1]))
+print('total distance : '+ str(total_distance)+'m')
 
-print('predict working time : '+str((total_distance/working_speed)/60))
-print('predict running time : '+str((total_distance/running_speed)/60))
+print('predict working time : '+str((total_distance/working_speed)/60)+'min')
+print('predict running time : '+str((total_distance/running_speed)/60)+'min')
